d271e7ce7f5471b808280efef0ae09c43050056d
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-09-11 23:52:31 +0300

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: This program prints all possible combinations of single-digit
+ *     * numbers in ascending order, separated by ", ".
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+int main(void)
+{
+	    int num;
+
+	        for (num = 0; num < 10; num++)
+			    {
+				            putchar(num + '0');
+					            if (num < 9)
+							            {
+									                putchar(',');
+											            putchar(' ');
+												            }
+						        }
+
+		    putchar('\n');
+
+		        return (0);
+}
