011087dd60f71afa0c9a6a72eabb8f9ed69a4f34
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-09-11 23:45:54 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,28 +1,17 @@
-include <stdio.h>
+#include <stdio.h>
 
-/**
- *  * main - Entry point
- *   *
- *    * Description: This program prints all hexadecimal numbers (0-9 and a-f)
- *     *              in lowercase, followed by a new line, using putchar.
- *      *
- *       * Return: Always 0 (Success)
- *        */
-int main(void)
-{
-	    char i;
+int main() {
+	  char i;
 
-	        for (i = '0'; i <= '9'; i++)
-			    {
-				            putchar(i);
-					        }
+	    for (i = '0'; i <= '9'; i++) {
+		        putchar(i);
+			  }
 
-		    for (i = 'a'; i <= 'f'; i++)
-			        {
-					        putchar(i);
-						    }
+	      for (i = 'a'; i <= 'f'; i++) {
+		          putchar(i);
+			    }
 
-		        putchar('\n');
+	        putchar('\n');
 
-			    return 0;
+		  return 0;
 }
