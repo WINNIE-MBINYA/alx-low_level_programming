2e9497b9750d702ee0e77576200d1b25a13b7a3c
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-09-11 23:14:19 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *  * Description: This program prints a sequence of characters representing
+ *   *              digits (0-9) and lowercase hexadecimal numbers (a-f),
+ *    *              followed by a newline character.
+ *     *
+ *      * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	   char i;
+		for (i = '0'; i <= '9'; i++)
+		putchar(i);
+		for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+		putchar('\n');
 
-int main() {
-	  char i;
-
-	    for (i = '0'; i <= '9'; i++) {
-		        putchar(i);
-			  }
-
-	      for (i = 'a'; i <= 'f'; i++) {
-		          putchar(i);
-			    }
-
-	        putchar('\n');
-
-		  return 0;
+		return (0);
 }
