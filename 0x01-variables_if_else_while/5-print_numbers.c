a0c4cbfb6228995a04c7b28c4e207868595ccdc8
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-09-11 20:27:31 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: This program prints single-digit numbers in base 10
+ *     *              starting from 0 and followed by a new line.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
+int main(void)
+{
+	    int number = 0;
+
+	        while (number <= 9)
+			    {
+				            putchar(number + '0');  // Convert and print as a character
+								    //         number++;
+								    //             }
+								    //
+								    //                 putchar('\n');
+								    //
+								    //                     return (0);
+								    //                     }
