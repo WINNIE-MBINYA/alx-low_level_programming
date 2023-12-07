0ce752053d9c8808848eeaa22ce24d63f8c8b3cd
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2023-12-07 23:59:22 +0300

    Tasks

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a position
+ * @h: ptr to the head of the list
+ * @idx: index of the list where new node should be added
+ * @n: data of new node
+ * 
+ * Return:  address of the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
